688bd71159e7aead6eb39376c16913ce4a0f1083
Yaroslav <yaroslavrokhmanov@gmail.com>
2021-09-20 22:02:14 +0200

    lesson 83

---
+++ lesson83.js
@@ -0,0 +1,17 @@
+// "use strict";
+
+// Основы работы с пользовательскими функциями в JavaScript
+
+//=======Задача 83.1
+function my_name(){
+    alert('Yaroslav');
+    }
+    my_name();
+
+//=======Задача 83.2
+function numbers(){
+    for(let i=0; i<100; i++){
+        console.log(i+1);
+        }
+    }
+        numbers();
