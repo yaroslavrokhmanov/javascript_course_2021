3ceec0445cdfa9464e5af325c202e3bb5bb6e957
Yaroslav <yaroslavrokhmanov@gmail.com>
2021-12-21 12:13:27 +0100

    finish lesson 96

--- lesson96.js
+++ lesson96.js
@@ -43,3 +43,27 @@ console.log(range(6, 12));
 
 // Задача 96.3
 // Сделайте функцию rangeRand, возвращающую массив, заполненный случайными целыми числами из заданного диапазона. При этом числа не должны повторяться. Для решения задачи используйте комбинацию функций range и shuffle.
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function rangeRand(num1, num2) {
+    let result = [];
+
+    for (let i = num1; i <= num2; i++) {
+        result.push(i);
+    }
+    return shuffle(result);
+}
+
+function shuffle(arr) {
+    let result = [];
+
+    while (arr.length > 0) {
+        let random = getRandomInt(0, arr.length - 1);
+        let elem = arr.splice(random, 1)[0];
+        result.push(elem);
+    }
+    return result;
+}
+console.log(rangeRand(10, 30));
